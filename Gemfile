5f4fc201ffe61131b56bd90ee600be5b017914b3
jzanutto <jzanutto@live.com>
2014-11-21 20:52:40 -0500

    Added basic GCM integration for local development

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'shotgun'
+gem 'gcm'
+gem 'tux'
--- router.rb
+++ router.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require './api/gcm_adapter'
 
 module Router
     class TimsRun < Sinatra::Base
+        attr_accessor :gcm
+        before do
+            @gcm = GCMAdapter.new
+        end
+
         get '/' do
-           halt 404
+           response = gcm.push(["-1"], {:stuff => "not stuff"})
+           response
         end
     end
 end
